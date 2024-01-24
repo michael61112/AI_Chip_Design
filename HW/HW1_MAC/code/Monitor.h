9cda664896c3152bf7e648a542f1724573969d41
michael61112 <michael61112@yahoo.com.tw>
2024-01-24 15:09:02 +0800

    HW1: Rename the Lab1 to HW1

---
+++ HW/HW1_MAC/code/Monitor.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "systemc.h"
+
+using namespace std;
+
+SC_MODULE(Monitor) {
+    sc_in<bool> rst;
+    sc_in_clk clock;
+    sc_in<sc_uint<4> > A, B;
+    sc_in<sc_uint<8> > C;
+    sc_in<sc_uint<9> > Y;
+    int cycle;
+
+    void run();
+
+    SC_CTOR(Monitor) {
+        cycle = 0;
+
+        SC_METHOD(run);
+        sensitive << clock.pos();
+    }
+};
